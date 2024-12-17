a5acbfeb0af53d8746186212f2b39a3e1e4bb762
Leonardo Souza <leonardosilva.souza@yahoo.com.br>
2024-12-17 20:47:36 -0300

    add flask_talisman with flask - Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_talisman import Talisman
 
+# Inicializa o app Flask
 app = Flask(__name__)
+
+# Configurações de segurança com Flask-Talisman
+csp = {
+    'default-src': '\'self\'',
+    'script-src': '\'self\'',
+    'style-src': '\'self\''
+}
+Talisman(app, content_security_policy=csp)
 
 @app.route("/")
 def pagina_inicial():
     return "App do Leonardo Silva Souza github: leosilvasouza"
+    
+# Inicializa a aplicação
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
