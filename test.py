a81b61e210628db0f456a8e1b9df8fd595b98188
Leonardo Souza <leonardosilva.souza@yahoo.com.br>
2024-12-17 21:12:19 -0300

    print to debug in test.py - Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ class Test(unittest.TestCase):
     def setUp(self):
         # Usa o contexto do Flask sem precisar do test_client
         app.config['TESTING'] = True
+        app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False  # Evita redirecionamentos indesejados
         self.client = app.test_client()
 
     def test_requisicao(self):
@@ -16,7 +17,9 @@ class Test(unittest.TestCase):
     def test_conteudo(self):
         # Verifica o conteúdo retornado pela página
         result = self.client.get('/')
+        print(result.data.decode('utf-8'))  # Adiciona um print para depuração
         self.assertEqual(result.data.decode('utf-8'), "App do Leonardo Silva Souza github: leosilvasouza")
 
 if __name__ == '__main__':
     unittest.main()
+
